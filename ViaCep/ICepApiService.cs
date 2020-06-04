d77acacb59741475a443dc8af22875d35f4c51ed
Rondinele Guimaraes <rondineleg@edu.univali.br>
2020-06-04 13:33:06 -0300

    Consumindo API do ViaCep  utilizando o C# e a biblioteca Refit.

---
+++ ViaCep/ICepApiService.cs
@@ -0,0 +1,11 @@
+﻿using Refit;
+using System.Threading.Tasks;
+
+namespace ViaCep
+{
+    public interface ICepApiService
+    {
+        [Get("/ws/{cep}/json/")]
+        Task<CepResponse> BuscaEndereco(string cep);
+    }
+}
---
+++ ViaCep/Program.cs
@@ -0,0 +1,35 @@
+﻿using Refit;
+using System;
+using System.Threading.Tasks;
+
+namespace ViaCep
+{
+    class Program
+    {
+        static async Task Main()
+        {
+            try
+            {
+                var cepClient = RestService.For<ICepApiService>("https://viacep.com.br");
+                
+                Console.WriteLine("Informeo cep : ");
+
+                var cepInformado = Console.ReadLine().ToString();
+
+                var address = await cepClient.BuscaEndereco(cepInformado);
+
+                Console.WriteLine($"Logradouro : {address.Logradouro}");
+                Console.WriteLine($"Complemento : {address.Complemento}");
+                Console.WriteLine($"Bairro : {address.Bairro}");
+                Console.WriteLine($"UF : {address.Uf}");
+                Console.WriteLine($"Cidade : {address.Localidade}");
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine("Erro na consulta do cep : " + ex.Message);
+            }
+            
+        }
+    }
+}
